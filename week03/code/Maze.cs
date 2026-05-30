326af8f574cb8cceea63aad26be46d2faa4e9f1c
Luis Chavez <luisernestochavezleal@gmail.com>
2026-05-30 19:05:47 -0400

    Complete Week 3 assignments including Sets, Maps, and Maze tracking

--- week03/code/Maze.cs
+++ week03/code/Maze.cs
@@ -27,39 +27,91 @@ public class Maze
 
     // TODO Problem 4 - ADD YOUR CODE HERE
     /// <summary>
-    /// Check to see if you can move left.  If you can, then move.  If you
+    /// Check to see if we can move left.  If we can, then move.  If we
     /// can't move, throw an InvalidOperationException with the message "Can't go that way!".
     /// </summary>
     public void MoveLeft()
     {
         // FILL IN CODE
+        // Get the status of the current position from the maze dictionary
+        var currentCell = _mazeMap[(_currX, _currY)];
+
+        // Check if moving left is allowed (index 0 represents Left)
+        if (currentCell[0])
+        {
+            _currX--;
+        }
+        else
+        {
+            // Throw an InvalidOperationException
+            throw new InvalidOperationException("Can't go that way!");
+        }
     }
 
     /// <summary>
-    /// Check to see if you can move right.  If you can, then move.  If you
+    /// Check to see if we can move right.  If we can, then move.  If we
     /// can't move, throw an InvalidOperationException with the message "Can't go that way!".
     /// </summary>
     public void MoveRight()
     {
         // FILL IN CODE
+        // Get the status of the current position from the maze dictionary
+        var currentCell = _mazeMap[(_currX, _currY)];
+
+        // Check if moving right is allowed (index 1 represents Right)
+        if (currentCell[1])
+        {
+            _currX++;
+        }
+        else
+        {
+            // Throw an InvalidOperationException
+            throw new InvalidOperationException("Can't go that way!");
+        }
     }
 
     /// <summary>
-    /// Check to see if you can move up.  If you can, then move.  If you
+    /// Check to see if we can move up.  If we can, then move.  If we
     /// can't move, throw an InvalidOperationException with the message "Can't go that way!".
     /// </summary>
     public void MoveUp()
     {
         // FILL IN CODE
+        // Get the status of the current position from the maze dictionary
+        var currentCell = _mazeMap[(_currX, _currY)];
+
+        // Check if moving up is allowed (index 2 represents Up)
+        if (currentCell[2])
+        {
+            _currY--;
+        }
+        else
+        {
+            // Throw an InvalidOperationException
+            throw new InvalidOperationException("Can't go that way!");
+        }
     }
 
     /// <summary>
-    /// Check to see if you can move down.  If you can, then move.  If you
+    /// Check to see if we can move down.  If we can, then move.  If we
     /// can't move, throw an InvalidOperationException with the message "Can't go that way!".
     /// </summary>
     public void MoveDown()
     {
         // FILL IN CODE
+        // Get the status of the current position from the maze dictionary
+        var currentCell = _mazeMap[(_currX, _currY)];
+
+        // Check if moving down is allowed (index 3 represents Down)
+        if (currentCell[3])
+        {
+            _currY++;
+        }
+        else
+        {
+            // Throw an InvalidOperationException
+            throw new InvalidOperationException("Can't go that way!");
+        }
     }
 
     public string GetStatus()
--- week03/code/SetsAndMaps.cs
+++ week03/code/SetsAndMaps.cs
@@ -22,7 +22,30 @@ public static class SetsAndMaps
     public static string[] FindPairs(string[] words)
     {
         // TODO Problem 1 - ADD YOUR CODE HERE
-        return [];
+        var history = new HashSet<string>();
+        var results = new List<string>();
+
+        foreach (var pair in words)
+        {
+            // We created the word mirror (backwards) of the current pair by switching the order of the letters
+            string reverse = "" + pair[1] + pair[0];
+
+            // Check if the reversed version is already in our history
+            if (history.Contains(reverse))
+            {
+                // We add it to the results list with the required format
+                results.Add($"{reverse} & {pair}");
+            }
+            else
+            {
+                // If its pair is not there yet, we save the current word
+                // so that the following words can search for it
+                history.Add(pair);
+            }
+        }
+
+        // We convert the list to a classic string arrangement as required by the method
+        return results.ToArray();
     }
 
     /// <summary>
@@ -43,6 +66,17 @@ public static class SetsAndMaps
         {
             var fields = line.Split(",");
             // TODO Problem 2 - ADD YOUR CODE HERE
+            var degree = fields[3];
+            if (degrees.ContainsKey(degree))
+            {
+                degrees[degree]++;
+            }
+
+            else
+            {
+                degrees[degree] = 1;
+            }
+
         }
 
         return degrees;
@@ -67,7 +101,52 @@ public static class SetsAndMaps
     public static bool IsAnagram(string word1, string word2)
     {
         // TODO Problem 3 - ADD YOUR CODE HERE
-        return false;
+        // Remove spaces and lower case all letters
+        word1 = word1.Replace(" ", "").ToLower();
+        word2 = word2.Replace(" ", "").ToLower();
+
+        // Check if the two words has the same length
+        if (word1.Length != word2.Length)
+        {
+            return false;
+        }
+
+        var letterCount = new Dictionary<char, int>();
+
+        // Verify if each letter is in the Dictionary
+        foreach (char letter in word1)
+        {
+            if (letterCount.ContainsKey(letter))
+            {
+                letterCount[letter]++;
+            }
+            else
+            {
+                letterCount[letter] = 1;
+            }
+        }
+
+        // Check if each letter in word2 is once in the dictionary
+        foreach (char letter in word2)
+        {
+            if (letterCount.ContainsKey(letter))
+            {
+                letterCount[letter]--;
+
+                // If the count letter is less than 0, it is not an anagram
+                if (letterCount[letter] < 0)
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                // If the letter doesn't even exist in the first count, it's not an anagram
+                return false;
+            }
+        }
+
+        return true;
     }
 
     /// <summary>
@@ -101,6 +180,6 @@ public static class SetsAndMaps
         // on those classes so that the call to Deserialize above works properly.
         // 2. Add code below to create a string out each place a earthquake has happened today and its magitude.
         // 3. Return an array of these string descriptions.
-        return [];
+        return Array.Empty<string>();
     }
 }
